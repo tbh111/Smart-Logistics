6f27715a2dcb601860f438f0f34db41e62273fd1
tbh111 <1308751722@qq.com>
2021-02-11 12:28:10 +0800

    Add files via upload

---
+++ vision/CMakeFiles/cv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv.dir/src/main.cpp.o"
+  "bin/cv.pdb"
+  "bin/cv"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
